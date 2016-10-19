a96bbed6af4f31b97b09865349b8d11d3a311774
David Stockton <dave@davidstockton.com>
2016-10-19 00:30:36 -0700

    Read-only Fizzbuzz API

---
+++ module/Fizzbuzz/src/V1/Rest/FizzBuzzApi/FizzBuzzApiCollection.php
@@ -0,0 +1,8 @@
+<?php
+namespace Fizzbuzz\V1\Rest\FizzBuzzApi;
+
+use Zend\Paginator\Paginator;
+
+class FizzBuzzApiCollection extends Paginator
+{
+}
---
+++ module/Fizzbuzz/src/V1/Rest/FizzBuzzApi/FizzBuzzApiEntity.php
@@ -0,0 +1,40 @@
+<?php
+namespace Fizzbuzz\V1\Rest\FizzBuzzApi;
+
+class FizzBuzzApiEntity
+{
+    private $number;
+    private $word;
+
+    /**
+     * @return mixed
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
+    /**
+     * @param mixed $number
+     */
+    public function setNumber($number)
+    {
+        $this->number = $number;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getWord()
+    {
+        return $this->word;
+    }
+
+    /**
+     * @param mixed $word
+     */
+    public function setWord($word)
+    {
+        $this->word = $word;
+    }
+}
